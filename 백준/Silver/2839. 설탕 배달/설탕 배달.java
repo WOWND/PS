b710a8cc9291b499ad907a4a91406d06852d0192
Jaejung Park <jjp910123@gmail.com>
2025-04-02 23:08:09 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,25 +1,29 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class Main {
-    static final int MAX = 100_000;
-
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        int N = Integer.parseInt(scanner.nextLine());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] dp = new int[N + 1];
+        int max = 1_000_000_000;
+        int N = Integer.parseInt(reader.readLine());
 
-        dp[1] = MAX;
-        dp[2] = MAX;
+        int[] dp = new int[N+1];
+        dp[0] = max;
+        dp[1] = max;
+        dp[2] = max;
         dp[3] = 1;
-        if(N>=4) dp[4] = MAX;
-        if(N>=5) dp[5] = 1;
+        if (N >= 4) {
+            dp[4] = max;
+        }
+        if (N >= 5) {
+            dp[5] = 1;
+        }
 
         for (int i = 6; i <= N; i++) {
             dp[i] = Math.min(dp[i - 3], dp[i - 5]) + 1;
         }
-
-        System.out.println(dp[N] >= MAX ? -1 : dp[N]);
+        System.out.println(dp[N] >= max ? -1 : dp[N]);
     }
 }
