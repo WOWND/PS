b58ad2733ff05407a4ab3ec9d3d32bf0e1b3ca11
Jaejung Park <jjp910123@gmail.com>
2025-04-06 01:45:50 +0900

    [Silver III] Title: 소수 구하기, Time: 248 ms, Memory: 21060 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -8,30 +8,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-        reader.close();
-        
         int M = Integer.parseInt(tokenizer.nextToken());
         int N = Integer.parseInt(tokenizer.nextToken());
 
         boolean[] isPrime = new boolean[N + 1];
         Arrays.fill(isPrime, true);
+        isPrime[0] = false;
         isPrime[1] = false;
 
+        //아리스토테네스의 체
         for (int i = 2; i <= N; i++) {
-            if (isPrime[i]) {
-                for (int j = 2; i * j <= N; j++) {
-                    isPrime[i * j] = false;
-                }
+            if (!isPrime[i]) { //소수가 아니면
+                continue;
+            }
+            //소수의 배수들 제거
+            for (int j = i * 2; j <= N; j += i) {
+                isPrime[j] = false;
             }
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = M; i <= N; i++) {
-            if (isPrime[i]) {
-                sb.append(i).append("\n");
+            if(isPrime[i]){
+                sb.append(i).append('\n');
             }
         }
-        
         System.out.print(sb);
     }
 }
