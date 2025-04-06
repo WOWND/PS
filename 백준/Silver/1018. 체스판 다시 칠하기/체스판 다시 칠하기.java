28b68e656de0ee4b79816f26f5d2a62aaefa1521
Jaejung Park <jjp910123@gmail.com>
2025-04-06 23:03:07 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 108 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -4,47 +4,45 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[][] arr;
-
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+
         int N = Integer.parseInt(tokenizer.nextToken());
         int M = Integer.parseInt(tokenizer.nextToken());
 
-        arr = new boolean[N][M];
+        boolean[][] boards = new boolean[N][M]; //화이트: 1, 블랙: 0
         for (int i = 0; i < N; i++) {
-            char[] str = reader.readLine().toCharArray();
+            String str = reader.readLine();
             for (int j = 0; j < M; j++) {
-                arr[i][j] = str[j] == 'W';
+                boards[i][j] = str.charAt(j) == 'W';
             }
         }
 
-        int answer = Integer.MAX_VALUE;
+        int answer = 100000;
         for (int i = 0; i < N - 7; i++) {
             for (int j = 0; j < M - 7; j++) {
-                answer = Math.min(find(i, j), answer);
+                answer = Math.min(answer, solution(boards, i, j));
             }
         }
-
         System.out.println(answer);
     }
 
-    public static int find(int x, int y) {
+    static int solution(boolean[][] boards,int x, int y) {
+        boolean color = boards[x][y];
         int count = 0;
-        boolean TF = arr[x][y];
 
         for (int i = x; i < x + 8; i++) {
             for (int j = y; j < y + 8; j++) {
-
-                if (arr[i][j] != TF) { //올바른 색이 아니면 count 증가
+                if (boards[i][j] != color) {
                     count++;
                 }
-                TF = !TF;
+                color = !color;
             }
-            TF = !TF;
+            color = !color;
         }
-        return Math.min(count, 64 - count);
+
+        return Math.min(count,64-count);
     }
 }
