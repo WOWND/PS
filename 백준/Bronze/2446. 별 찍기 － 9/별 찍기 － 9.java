6f7b9f8305ecdaf818c96d6bdb704f9b6d6189df
Jaejung Park <jjp910123@gmail.com>
2025-04-23 18:06:27 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(reader.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = T; i > 1; i--) {
+            sb.append(" ".repeat(T - i)).append("*".repeat(2 * i - 1)).append("\n");
+        }
+
+        for (int i = 1; i <= T; i++) {
+            sb.append(" ".repeat(T - i)).append("*".repeat(2 * i - 1)).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
