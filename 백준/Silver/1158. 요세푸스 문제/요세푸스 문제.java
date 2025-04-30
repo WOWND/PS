5afaa36f9827c943562bcbdf42b1cd5775258d12
Jaejung Park <jjp910123@gmail.com>
2025-04-30 10:27:25 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 200 ms, Memory: 18628 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,46 +1,89 @@
 import java.io.*;
-import java.security.Key;
-import java.util.LinkedList;
-import java.util.ListIterator;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+
 public class Main {
+
+    static int[] dat;
+    static int[] pre;
+    static int[] nxt;
+
+    static int size = 0;
+
+    static int unused = 1;
+
+    static void init(int size) {
+        size++;
+        dat = new int[size];
+        pre = new int[size];
+        nxt = new int[size];
+    }
+
+    static void insert(int addr, int val) {
+        dat[unused] = val;
+        pre[unused] = addr;
+        nxt[unused] = nxt[addr];
+
+
+        if (nxt[addr] != -1) {
+            pre[nxt[addr]] = unused;
+        }
+        nxt[addr] = unused;
+
+        unused++;
+        size++;
+    }
+
+    static int erase(int addr) {
+        if (nxt[addr] == -1) { //끝값이면
+            nxt[pre[addr]] = nxt[0]; //첫값으로
+        } else {
+            nxt[pre[addr]] = nxt[addr];
+        }
+
+        if (nxt[addr] != -1) {
+            pre[nxt[addr]] = pre[addr];
+        }
+
+        size--;
+        return dat[addr];
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
 
-        LinkedList<Integer> list = new LinkedList<>();
-
         int N = Integer.parseInt(tokenizer.nextToken());
         int K = Integer.parseInt(tokenizer.nextToken());
+        init(N);
+
+        Arrays.fill(dat,-1);
+        Arrays.fill(pre,-1);
+        Arrays.fill(nxt,-1);
+
         for (int i = 1; i <= N; i++) {
-            list.add(i);
+            dat[i] = i;
+            pre[i] = (i == 1) ? N : i - 1;
+            nxt[i] = (i == N) ? 1 : i + 1;
         }
-        ListIterator<Integer> cursor = list.listIterator();
+
+        size = N;
+        int addr = 1;
 
         StringBuilder sb = new StringBuilder();
         sb.append('<');
-        while (!list.isEmpty()) {
-            for (int i = 0; i < K; i++) {
-                if (cursor.hasNext()) {
-                    cursor.next();
-                } else {
-                    cursor = list.listIterator();
-                    cursor.next();
-                }
+        while (size > 0) {
+            for (int i = 1; i < K; i++) {
+                addr = nxt[addr];
             }
-
-            if (cursor.hasPrevious()) {
-                sb.append(cursor.previous());
-                cursor.remove();
-            }
-
-            if (!list.isEmpty()) {
+            sb.append(erase(addr));
+            if (size > 0) {
                 sb.append(", ");
             }
+            addr = nxt[addr];
         }
-
         sb.append('>');
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
