25915179e63bf3b575fe4b60937d4b7e9416ef05
Jaejung Park <jjp910123@gmail.com>
2025-05-17 02:04:06 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 192 ms, Memory: 27076 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
+
+public class Main {
+    static int[] board;
+
+
+    public static void main(String[] args) throws IOException {
+        int N = read();
+        int K = read();
+        board = new int[200_001];
+        Arrays.fill(board, -1);
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(N);
+        board[N] = 0;
+
+
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
+            if (x == K) {
+                System.out.println(board[x]);
+                return;
+            }
+
+            int[] dx = {x * 2, x - 1, x + 1};
+            for (int i = 0; i < 3; i++) {
+                int nx = dx[i];
+
+                if (nx < 0 || nx > 200_000) {
+                    continue;
+                }
+
+
+                if (i == 0) {
+                    if (board[nx] != -1 && board[nx] <= board[x]) {
+                        continue;
+                    }
+                    queue.offer(nx);
+                    board[nx] = board[x];
+                    continue;
+                }
+                if (board[nx] != -1 &&board[nx] <= board[x]) {
+                    continue;
+                }
+                queue.offer(nx);
+                board[nx] = board[x] + 1;
+            }
+        }
+    }
+
+    public static int read() throws IOException {
+        int d = System.in.read();
+        boolean isMinus = false;
+
+        if (d == '-') {
+            isMinus = true;
+            d = System.in.read();
+        }
+        int o = d - '0';
+
+        while ((d = System.in.read()) > ' ') {
+            o = (o << 3) + (o << 1) + (d - '0');
+        }
+        return isMinus ? -o : o;
+
+    }
+}
+
+
