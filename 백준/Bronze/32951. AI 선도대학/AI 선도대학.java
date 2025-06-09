41991bcc09cc0fb32d9f5ab95f922a65c2610477
Jaejung Park <jjp910123@gmail.com>
2025-06-09 23:28:04 +0900

    [Bronze V] Title: AI 선도대학, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        System.out.println(read() - 2024);
+    }
+
+    public static int read() throws IOException {
+        int d = System.in.read();
+        boolean isMinus = false;
+
+        if (d == '-') {
+            isMinus = true;
+            d = System.in.read();
+        }
+        int o = d - '0';
+
+        while ((d = System.in.read()) > ' ') {
+            o = (o << 3) + (o << 1) + (d - '0');
+        }
+        return isMinus ? -o : o;
+
+    }
+}
