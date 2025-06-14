f776bfb4858cc6795bcba8d752a940f9466ff545
Jaejung Park <jjp910123@gmail.com>
2025-06-14 21:47:03 +0900

    [Bronze V] Title: 노트북 세 대를 가지고 왔다, Time: 108 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        int T1 = Integer.parseInt(tokenizer.nextToken());
+        int T2 = Integer.parseInt(tokenizer.nextToken());
+        System.out.println(Math.min(T1, T2));
+    }
+}
