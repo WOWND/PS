eada89ca04bd0568c795def82423d63b4bc7e348
Jaejung Park <jjp910123@gmail.com>
2025-07-02 23:43:24 +0900

    [Bronze V] Title: 사분면 고르기, Time: 96 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(reader.readLine());
+        int y = Integer.parseInt(reader.readLine());
+
+        if (x > 0) {
+            if (y > 0) System.out.println(1);
+            else System.out.println(4);
+        } else {
+            if (y > 0) System.out.println(2);
+            else System.out.println(3);
+        }
+    }
+}
