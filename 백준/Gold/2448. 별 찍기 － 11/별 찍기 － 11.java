8b0d3009c93cbf1bb8069778b1212295ce1128f4
Jaejung Park <jjp910123@gmail.com>
2025-07-29 23:53:21 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 476 ms, Memory: 145564 KB -BaekjoonHub

---
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static char[][] answer;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(reader.readLine());
+        reader.close();
+
+        answer = new char[N][N * 2 - 1];
+
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(answer[i], ' ');
+        }
+
+
+        func(0, N - 1, N / 3);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N * 2 - 1; j++) {
+                sb.append(answer[i][j]);
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb);
+    }
+
+
+    public static void func(int r, int c, int n) {
+        if (n == 1) {
+            answer[r][c] = '*';
+            answer[r + 1][c - 1] = '*';
+            answer[r + 1][c + 1] = '*';
+            for (int i = 0; i < 5; i++) {
+                answer[r + 2][c - 2 + i] = '*';
+            }
+            return;
+        }
+
+
+        func(r, c, n / 2);
+        func(r + n / 2 * 3, c - (3 * (n / 2)), n / 2);
+        func(r + n / 2 * 3, c + (3 * (n / 2)), n / 2);
+
+    }
+}
