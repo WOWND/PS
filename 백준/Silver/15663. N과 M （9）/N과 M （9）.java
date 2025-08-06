13f3e7c0861a8bf72cf15144e787630419007abb
Jaejung Park <jjp910123@gmail.com>
2025-08-07 04:28:36 +0900

    [Silver II] Title: N과 M (9), Time: 232 ms, Memory: 32656 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static int[] answer = new int[10];
+    static int[] nums;
+    static HashSet<String> set = new HashSet<>();
+    static boolean[] isUsed = new boolean[10];
+    static StringBuilder stringBuilder = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        N = Integer.parseInt(tokenizer.nextToken());
+        M = Integer.parseInt(tokenizer.nextToken());
+
+        nums = new int[N];
+        tokenizer = new StringTokenizer(reader.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+        Arrays.sort(nums);
+        reader.close();
+
+        func(0);
+        System.out.print(stringBuilder);
+    }
+
+    static void func(int count) {
+        if (count == M) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < M; i++) {
+                sb.append(answer[i]).append(' ');
+            }
+            String s = sb.toString();
+            if (!set.contains(s)) {
+                set.add(s);
+                sb.append('\n');
+                stringBuilder.append(sb);
+            }
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!isUsed[i]) {
+                answer[count] = nums[i];
+                isUsed[i] = true;
+                func(count + 1);
+                isUsed[i] = false;
+            }
+        }
+    }
+}
