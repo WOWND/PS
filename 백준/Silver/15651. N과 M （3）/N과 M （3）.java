7496e46950fcec3a3c820aebc2c7e6be87fc2b33
Jaejung Park <jjp910123@gmail.com>
2025-08-07 00:55:55 +0900

    [Silver III] Title: N과 M (3), Time: 396 ms, Memory: 68180 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static int[] answer = new int[10];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        reader.close();
+
+        N = Integer.parseInt(tokenizer.nextToken());
+        M = Integer.parseInt(tokenizer.nextToken());
+
+        func(0);
+        System.out.print(sb);
+    }
+
+    static void func(int count) {
+        if (count == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(answer[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            answer[count] = i;
+            func( count + 1);
+        }
+    }
+
+}
