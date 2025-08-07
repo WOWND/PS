ec5842187773ea14bcf8f75f809a70c7017a2fba
Jaejung Park <jjp910123@gmail.com>
2025-08-07 21:45:38 +0900

    [Silver II] Title: N과 M (12), Time: 116 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static int[] answer = new int[10];
+    static ArrayList<Integer> nums = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        N = Integer.parseInt(tokenizer.nextToken());
+        M = Integer.parseInt(tokenizer.nextToken());
+
+        tokenizer = new StringTokenizer(reader.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(tokenizer.nextToken());
+            if (!nums.contains(num)) {
+                nums.add(num);
+            }
+        }
+        nums.sort(null);
+        reader.close();
+
+        func(0, 0);
+        System.out.print(sb);
+    }
+
+    static void func(int index, int count) {
+        if (count == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(answer[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+
+        for (int i = index; i < nums.size(); i++) {
+            answer[count] = nums.get(i);
+            func(i, count + 1);
+        }
+    }
+}
