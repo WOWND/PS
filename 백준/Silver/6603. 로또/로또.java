64eaaf144136054ce14e3c12c91e766524b88fc7
Jaejung Park <jjp910123@gmail.com>
2025-08-09 23:27:15 +0900

    [Silver II] Title: 로또, Time: 108 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] numbers;
+    static boolean[] isUsed;
+    static int[] answer = new int[6];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        int k;
+        while ((k = Integer.parseInt(tokenizer.nextToken())) != 0) {
+            numbers = new int[k];
+            isUsed = new boolean[k];
+
+            for (int i = 0; i < k; i++) {
+                numbers[i] = Integer.parseInt(tokenizer.nextToken());
+            }
+
+            func(0, 0);
+            tokenizer = new StringTokenizer(reader.readLine());
+            sb.append('\n');
+        }
+        System.out.print(sb);
+
+    }
+
+    static void func(int index, int count) {
+        if (count == 6) {
+            for (int i = 0; i < 6; i++) {
+                sb.append(answer[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        
+
+        for (int i = index; i < numbers.length; i++) {
+            answer[count] = numbers[i];
+            isUsed[i] = true;
+            func(i + 1, count + 1);
+            isUsed[i] = false;
+        }
+    }
+}
