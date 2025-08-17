98af99fd5bfc353979a9468c33f4d33218fcb888
Jaejung Park <jjp910123@gmail.com>
2025-08-18 03:06:12 +0900

    [Gold III] Title: 감시, Time: 284 ms, Memory: 22608 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,25 +7,18 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int r, c, count;
-    static char[][] board;
-    static boolean[][][] visited;
+    static Point[] cctv = new Point[10];
+    static int r, c, size;
+
+
+    static int[] dx = {1, 0, -1, 0, 1, 0, -1, 0}; //하 좌 상 우
+    static int[] dy = {0, -1, 0, 1, 0, -1, 0, 1}; //하 좌 상 우
+
+    static char[][] board1 = new char[10][10]; //원본
+    static char[][] board2 = new char[10][10];
+
     static int min = Integer.MAX_VALUE;
 
-    static int[] dx = {1, 0, -1, 0, 1, 0, -1, 0}; //하,좌, 상, 우
-    static int[] dy = {0, -1, 0, 1, 0, -1, 0, 1};
-
-    static Node[] CCTV = new Node[8];
-    static class Node {
-        int x, y;
-        char type;
-
-        public Node(int x, int y, char type) {
-            this.x = x;
-            this.y = y;
-            this.type = type;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -33,90 +27,70 @@ public class Main {
         r = Integer.parseInt(tokenizer.nextToken());
         c = Integer.parseInt(tokenizer.nextToken());
 
-        board = new char[r][c];
         for (int i = 0; i < r; i++) {
             String str = reader.readLine();
             for (int j = 0; j < c; j++) {
-                char c = str.charAt(j * 2);
+                int c = str.charAt(2 * j);
                 if (c != '0' && c != '6') {
-                    CCTV[count++] = new Node(i, j, c);
+                    cctv[size++] = new Point(i, j);
                 }
-                board[i][j] = c;
+                board1[i][j] = str.charAt(2 * j);
             }
         }
-        visited = new boolean[count][][];
 
-        func(0);
-        System.out.println(min);
-    }
+        // 회전 방향 4개를 4진법을 통해 모든 경우의 수를 나타낼 수 있음 크기는 2^k (CCTV 개수)
+        for (int i = 0; i < 1 << 2 * size; i++) {
+            for (int j = 0; j < r; j++) { //원상복구
+                System.arraycopy(board1[j], 0, board2[j], 0, c);
+            }
 
-    private static void func(int cur) {
-        if (cur == count) {
-            int result = 0;
+            int tmp = i;
+            for (int j = 0; j < size; j++) {
+                int dir = tmp % 4;
+                tmp /= 4;
 
-            for (int i = 0; i < r; i++) {
-                A: for (int j = 0; j < c; j++) {
-                    if (board[i][j] == '0') {
-                        for (int k = 0; k < count; k++) {
-                            if (visited[k][i][j]) { //방문한적 있으면 사각지대 x
-                                continue A;
-                            }
-                        }
-                        result++;
+                Point cur = cctv[j];
+                int x = cur.x;
+                int y = cur.y;
+
+                if (board1[x][y] == '1') {
+                    visit(x, y, dir);
+                } else if (board1[x][y] == '2') {
+                    visit(x, y, dir);
+                    visit(x, y, dir + 2);
+                }else if (board1[x][y] == '3') {
+                    visit(x, y, dir);
+                    visit(x, y, dir + 1);
+                } else if (board1[x][y] == '4') {
+                    visit(x, y, dir);
+                    visit(x, y, dir + 1);
+                    visit(x, y, dir + 2);
+                } else {
+                    visit(x, y, dir + 1);
+                    visit(x, y, dir + 2);
+                    visit(x, y, dir + 3);
+                    visit(x, y, dir + 4);
+                }
+            }
+
+            int cnt = 0;
+            for (int k = 0; k < r; k++) {
+                for (int l = 0; l < c; l++) {
+                    if (board2[k][l] == '0') {
+                        cnt++;
                     }
                 }
             }
-            min = Math.min(min, result);
-            return;
+            min = Math.min(min, cnt);
         }
-
-        Node node = CCTV[cur];
-        if (node.type == '5') {
-            visited[cur] = new boolean[r][c];
-            visit(cur, node, dx[0], dy[0]);
-            visit(cur, node, dx[1], dy[1]);
-            visit(cur, node, dx[2], dy[2]);
-            visit(cur, node, dx[3], dy[3]);
-            func(cur + 1);
-        } else if (node.type == '4') {
-            for (int i = 0; i < 4; i++) {
-                visited[cur] = new boolean[r][c];
-                visit(cur, node, dx[i], dy[i]);
-                visit(cur, node, dx[i + 1], dy[i + 1]);
-                visit(cur, node, dx[i + 2], dy[i + 2]);
-                func(cur + 1);
-            }
-        }else if (node.type == '3') {
-            for (int i = 0; i < 4; i++) {
-                visited[cur] = new boolean[r][c];
-                visit(cur, node, dx[i], dy[i]);
-                visit(cur, node, dx[i + 1], dy[i + 1]);
-                func(cur + 1);
-            }
-        }else if (node.type == '2') {
-            for (int i = 0; i < 2; i++) {
-                visited[cur] = new boolean[r][c];
-                visit(cur, node, dx[i], dy[i]);
-                visit(cur, node, dx[i + 2], dy[i + 2]);
-                func(cur + 1);
-            }
-        }else{
-            for (int i = 0; i < 4; i++) {
-                visited[cur] = new boolean[r][c];
-                visit(cur, node, dx[i], dy[i]);
-                func(cur + 1);
-            }
-        }
+        System.out.println(min);
     }
 
-    private static void visit(int cur, Node node, int dx, int dy) {
-        int nx = node.x + dx;
-        int ny = node.y + dy;
-
-        while (nx >= 0 && nx < r && ny >= 0 && ny < c && board[nx][ny] != '6') {
-            visited[cur][nx][ny] = true;
-            nx += dx;
-            ny += dy;
+    private static void visit(int x, int y, int dir) {
+        while (x >= 0 && x < r && y >= 0 && y < c && board2[x][y] != '6') {
+            board2[x][y] = '7';
+            x += dx[dir];
+            y += dy[dir];
         }
     }
 }
