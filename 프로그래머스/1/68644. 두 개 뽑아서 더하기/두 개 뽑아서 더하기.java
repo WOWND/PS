c7bc13d219f40ce50dbfa3bcb592765a13f53eda
Park Jaejung <jjp910123@gmail.com>
2026-08-03 16:42:52 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 4.50 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        Set<Integer> answer = new TreeSet<>();
+
+        for (int i=0; i<numbers.length; i++) {
+            for (int j=i+1; j < numbers.length; j++) {
+                answer.add(numbers[i] + numbers[j]);
+            }
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
