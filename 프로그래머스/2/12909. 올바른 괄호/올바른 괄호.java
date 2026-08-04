2dc8db731d8bbf6f590f4e94dc2bf65ea63290ab
Park Jaejung <jjp910123@gmail.com>
2026-08-04 16:10:31 +0900

    [level 2] Title: 올바른 괄호, Time: 9.75 ms, Memory: 58.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    answer = false;
+                    break;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        if (!stack.isEmpty()) {
+            answer = false;
+        }
+        return answer;
+    }
+}
