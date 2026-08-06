87d549060cb8c18e6ea861251bc049c7229da237
Park Jaejung <jjp910123@gmail.com>
2026-08-06 18:05:46 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.72 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -3,30 +3,34 @@ import java.util.Queue;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int[] entryTimes = new int[truck_weights.length];
         Queue<Integer> q = new ArrayDeque<>();
+        int[] exitTimes = new int[truck_weights.length];
+        int time = 1;
         q.offer(0);
-        entryTimes[0] = 1;
+        exitTimes[0] = time + bridge_length;
         weight -= truck_weights[0];
         int next = 1;
+        time++;
 
-        int time = 0;
         while (!q.isEmpty()) {
-            time++;
-
-            int tn = q.peek();
-            if (time - entryTimes[tn] >= bridge_length) {
-                q.poll();
-                weight += truck_weights[tn];
+            while (!q.isEmpty() && exitTimes[q.peek()] <= time) {
+                weight += truck_weights[q.poll()];
             }
 
-            if (bridge_length > q.size() && next < truck_weights.length && weight >= truck_weights[next]) {
+            if (next < truck_weights.length && q.size() < bridge_length && truck_weights[next] <= weight) {
                 q.offer(next);
-                entryTimes[next] = time;
+                exitTimes[next] = time + bridge_length;
                 weight -= truck_weights[next];
                 next++;
+                time++;
+            } else {
+                if (!q.isEmpty()) {
+                    time = exitTimes[q.peek()];
+                }
             }
         }
+
         return time;
+
     }
 }
