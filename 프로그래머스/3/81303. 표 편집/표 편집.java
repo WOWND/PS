98d4b2569be02de9ba420916b3ef36d089ac742b
Park Jaejung <jjp910123@gmail.com>
2026-08-09 14:25:22 +0900

    [level 3] Title: 표 편집, Time: 106.64 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,43 +1,55 @@
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
     public String solution(int n, int k, String[] cmd) {
-        int[] up = new int[n + 2];
-        int[] down = new int[n + 2];
+        int[] up = new int[n+2];
+        int[] down = new int[n+2];
         k++;
-        for (int i = 0; i < n+2; i++) {
+        for(int i = 1; i <= n; i++) {
             up[i] = i - 1;
             down[i] = i + 1;
         }
+        
         Stack<Integer> stack = new Stack<>();
-
-        for (String s : cmd) {
+        for(String s : cmd) {
             String[] parts = s.split(" ");
-            if (parts.length > 1) {
+            
+            if(parts.length > 1) {
                 int dist = Integer.parseInt(parts[1]);
-
-                while (dist-- > 0) {
-                    k = parts[0].equals("D") ? down[k] : up[k];
+                
+                for(int i = 0; i < dist; i++) {
+                    if(parts[0].equals("U")) {
+                        k = up[k];
+                    } else {
+                        k = down[k];
+                    }
                 }
-            } else if (s.equals("C")) {
-                down[up[k]] = down[k];
-                up[down[k]] = up[k];
-                stack.push(k);
-
-                k = down[k] > n ? up[k] : down[k];
-            } else { // "Z"
-                int z = stack.pop();
-                down[up[z]] = z;
-                up[down[z]] = z;
+            } else {
+                if(parts[0].equals("C")) {
+                    stack.push(k);
+                    down[up[k]] = down[k];
+                    up[down[k]] = up[k];
+                    
+                    if(down[k] > n) {
+                        k = up[k];
+                    }else {
+                        k = down[k];
+                    }
+                } else { //Z
+                    int idx = stack.pop();
+                    down[up[idx]] = idx;
+                    up[down[idx]] = idx;
+                }
             }
         }
         
         char[] answer = new char[n];
         Arrays.fill(answer, 'O');
-        for (int i : stack) {
+        
+        for(int i : stack) {
             answer[i - 1] = 'X';
         }
+        
         return new String(answer);
     }
 }
