7feec52321ad84db4454c5e7be2434133a9cbb56
Park Jaejung <jjp910123@gmail.com>
2026-08-12 16:38:18 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.73 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,27 +1,28 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> q = new ArrayDeque<>();
         int[] exitTimes = new int[truck_weights.length];
+        Queue<Integer> q = new ArrayDeque<>();
         int time = 1;
         q.offer(0);
-        exitTimes[0] = time + bridge_length;
+        exitTimes[0] = bridge_length + time;
         weight -= truck_weights[0];
-        int next = 1;
-        time++;
 
+        int idx = 1;
+        time++;
+        
         while (!q.isEmpty()) {
-            while (!q.isEmpty() && exitTimes[q.peek()] <= time) {
-                weight += truck_weights[q.poll()];
+            if (time >= exitTimes[q.peek()]) {
+                int pt = q.poll();
+                weight += truck_weights[pt];
             }
 
-            if (next < truck_weights.length && q.size() < bridge_length && truck_weights[next] <= weight) {
-                q.offer(next);
-                exitTimes[next] = time + bridge_length;
-                weight -= truck_weights[next];
-                next++;
+            if (bridge_length > q.size() && idx < truck_weights.length && weight >= truck_weights[idx]) {
+                q.offer(idx);
+                weight -= truck_weights[idx];
+                exitTimes[idx] = bridge_length + time;
+                idx++;
                 time++;
             } else {
                 if (!q.isEmpty()) {
@@ -31,6 +32,5 @@ class Solution {
         }
 
         return time;
-
     }
 }
