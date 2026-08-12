a0e5dadd5212af3ed5b27bd2e24f2fe2352a64bb
Park Jaejung <jjp910123@gmail.com>
2026-08-12 16:01:02 +0900

    [level 2] Title: 기능개발, Time: 5.31 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,23 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> endTimeQ = new ArrayDeque<>();
+        Queue<Integer> q = new ArrayDeque<>();
         for (int i = 0; i < progresses.length; i++) {
-            endTimeQ.offer((int) Math.ceil((100. - progresses[i]) / speeds[i]));
+            q.offer((int) Math.ceil((100. - progresses[i]) / speeds[i]));
         }
 
         List<Integer> answer = new ArrayList<>();
-        while (!endTimeQ.isEmpty()) {
-            int curr = endTimeQ.poll();
+        while (!q.isEmpty()) {
+            int d = q.poll();
             int cnt = 1;
-
-            while (!endTimeQ.isEmpty() && endTimeQ.peek() <= curr) {
-                endTimeQ.poll();
+            
+            while (!q.isEmpty() && q.peek() <= d) {
+                q.poll();
                 cnt++;
             }
             answer.add(cnt);
         }
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
-    
 }
