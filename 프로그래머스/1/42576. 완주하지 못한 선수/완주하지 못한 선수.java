bace1cf2e705cb7a9e83f6072bb89d4699d70e4a
Park Jaejung <jjp910123@gmail.com>
2026-08-13 15:22:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 49.33 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,17 +4,14 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
         for (String s : completion) {
-            map.put(s, map.getOrDefault(s, 0) + 1);
+            map.merge(s, 1, Integer::sum);
         }
-        
+
         for (String s : participant) {
-            int cnt = map.getOrDefault(s, 0);
-            if (cnt == 0) {
+            if (map.merge(s, -1, Integer::sum) < 0) {
                 return s;
             }
-            map.put(s, cnt - 1);
         }
         return null;
     }
-    
 }
